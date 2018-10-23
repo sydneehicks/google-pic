c1153cab6da0d4880bd9e37dd3d769f126295183
sydre <sydre@DESKTOP-87CKSA8>
2018-10-23 10:45:17 -0400

    First Commit

--- Painting/src/paint.java
+++ Painting/src/paint.java
@@ -1,12 +1,11 @@
-package google.pic;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class paint {
 	public static void main(String [] args) {
 		JFrame f = new JFrame("Title");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		googlepic g = new googlepic();
+		painting g = new painting();
 		f.add(g);
 		f.setSize(400,250);
 		f.setVisible(true);
